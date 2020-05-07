a653959d8154f2aabebc4f279be906611ee03fb1
mkrueger12 <max@kruegers.com>
2020-05-07 16:08:57 -0600

    chore: fix typo

--- inventory/electric_data_func.py
+++ inventory/electric_data_func.py
@@ -6,7 +6,7 @@ def eiaScrape(BA, API_KEY):
 
     ''' Function will collect data from EIA website at hourly intervals.
         Must have EIA API Key from EIA website and specify the Balancing Authority (BA).
-        BA abbreviation must match abbreviations found here
+        BA abbreviation must match abbreviations found here:
         https://www.eia.gov/opendata/qb.php?category=2122628  '''
 
     url = f'http://api.eia.gov/series/?api_key={API_KEY}&series_id=EBA.{BA}-ALL.D.HL'
