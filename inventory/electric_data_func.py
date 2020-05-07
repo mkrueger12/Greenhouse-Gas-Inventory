f7391c2953164978ede44994bcda77e17a3939ae
mkrueger12 <max@kruegers.com>
2020-05-07 09:36:50 -0600

    feat: Add balancing authority to func

--- inventory/electric_data_func.py
+++ inventory/electric_data_func.py
@@ -1,13 +1,13 @@
 import requests
 
 # Create function to scrape EIA Data
-def eiaScrape(API_KEY):
+def eiaScrape(BA, API_KEY):
 
     ''' Function will collect data from EIA website at hourly intervals.
         Must have EIA API Key from EIA website and specify the Balancing Authority (BA).
-        Currently only supports collecting data from PSCO. '''
+        BA abbreviation must match abbreviations found here https://www.eia.gov/opendata/qb.php?category=2122628  '''
 
-    url = f'http://api.eia.gov/series/?api_key={API_KEY}&series_id=EBA.PSCO-ALL.D.HL'
+    url = f'http://api.eia.gov/series/?api_key={API_KEY}&series_id=EBA.{BA}-ALL.D.HL'
 
     data = requests.get(url=url).json()
 
