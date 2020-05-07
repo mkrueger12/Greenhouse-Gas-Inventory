5e2a6b5763c994d376643b639f102a9d7cd86cb7
mkrueger12 <max@kruegers.com>
2020-05-07 16:07:17 -0600

    chore: Add pandas

--- inventory/electric_data_func.py
+++ inventory/electric_data_func.py
@@ -1,13 +1,13 @@
 import requests
-
-API_KEY = '37ff344278e478d96077a269fe0966d4'
+import pandas as pd
 
 # Create function to scrape EIA Data
 def eiaScrape(BA, API_KEY):
 
     ''' Function will collect data from EIA website at hourly intervals.
         Must have EIA API Key from EIA website and specify the Balancing Authority (BA).
-        BA abbreviation must match abbreviations found here https://www.eia.gov/opendata/qb.php?category=2122628  '''
+        BA abbreviation must match abbreviations found here
+        https://www.eia.gov/opendata/qb.php?category=2122628  '''
 
     url = f'http://api.eia.gov/series/?api_key={API_KEY}&series_id=EBA.{BA}-ALL.D.HL'
 
