c05f45473d98dcd0fb2f696bb6ca146adf037907
mkrueger12 <max@kruegers.com>
2020-05-23 15:30:21 -0600

    Feat: Added net generation sources to func

--- inventory/scrape_functions.py
+++ inventory/scrape_functions.py
@@ -2,19 +2,21 @@ import requests
 import pandas as pd
 
 # Create function to scrape EIA Data
-def eiaScrape(BA, API_KEY):
+def eiaNetGen(BA, API_KEY, SOURCE):
 
-    ''' Function will collect data from EIA website at hourly intervals.
+    """ Function will collect net generation data from EIA website at hourly intervals.
         Must have EIA API Key from EIA website and specify the Balancing Authority (BA).
         BA abbreviation must match abbreviations found here:
-        https://www.eia.gov/opendata/qb.php?category=2122628  '''
+        https://www.eia.gov/opendata/qb.php?category=2122628.
+        Source Options include solar (SUN), wind (WND), coal (COL),
+        hydro (WAT), nat gas (NG), petroleum (OIL). """
 
-    url = f'http://api.eia.gov/series/?api_key={API_KEY}&series_id=EBA.{BA}-ALL.D.HL'
+    url = f'http://api.eia.gov/series/?api_key={API_KEY}&series_id=EBA.{BA}-ALL.NG.{SOURCE}.HL'
 
     data = requests.get(url=url).json()
 
     data = pd.DataFrame(data.get('series')[0].get('data'), columns=['Date', 'Load'])
 
-    data['Date'] = pd.to_datetime(dat['Date'])
+    data['Date'] = pd.to_datetime(data['Date'])
 
     return data
