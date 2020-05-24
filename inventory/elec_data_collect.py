50e0c63cf7ce46ddb64d595d5d69717a287a9ea9
mkrueger12 <max@kruegers.com>
2020-05-24 14:57:08 -0600

    feat: Add data collection

--- inventory/elec_data_collect.py
+++ inventory/elec_data_collect.py
@@ -7,4 +7,12 @@ API_KEY = EIA_API_KEY
 # Collect Data
 solar = eiaNetGen(BA, API_KEY, 'SUN')
 wind = eiaNetGen(BA, API_KEY, 'WND')
+col = eiaNetGen(BA, API_KEY, 'COL')
+hydro = eiaNetGen(BA, API_KEY, 'WAT')
+natgas = eiaNetGen(BA, API_KEY, 'NG')
+petrol = eiaNetGen(BA, API_KEY, 'OIL')
 all = eiaNetGen(BA, API_KEY, 'ALL')
+
+# Append Data
+
+
